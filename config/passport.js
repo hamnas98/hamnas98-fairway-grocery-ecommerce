d8436a5e87c775b2e7153f8568d88251c07e2c43
hamnas98 <hamnascp98@gmail.com>
2025-02-13 09:46:24 +0530

    updated google auth local

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,9 @@ const env = require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://fairwaygrocery.shop/auth/google/callback"
+    callbackURL: process.env.NODE_ENV === 'production' 
+        ? "https://fairwaygrocery.shop/auth/google/callback"
+        : "http://localhost:3000/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Check if user already exists
